9d9930903dbbd15214e83ea01700c2fb783d0a68
kimhongzhang323 <kim.hong.zhang323@gmail.com>
2024-07-23 16:29:11 +0800

    bool_check_win()

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -4,14 +4,6 @@
 
 using namespace std;
 
-int main() {
-    char board[3][3] = { { ' ', ' ', ' ' }, 
-                         { ' ', ' ', ' ' }, 
-                         { ' ', ' ', ' ' } };
-    drawBoard(board);
-    return 0;
-}
-
 void drawBoard(char board[3][3]) 
 { 
     cout << "-------------\n"; 
@@ -23,3 +15,31 @@ void drawBoard(char board[3][3])
         cout << "\n-------------\n"; 
     } 
 }
+
+bool check_win(char board[3][3], char player){
+    for (int i = 0; i<3; i++){
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player){
+            return true;
+        }
+
+        if (board[0][i] == player && board[1][i] == player && board[2][i] == player){
+            return true;
+        }
+    }
+}
+
+int main() {
+    char board[3][3] = { { ' ', ' ', ' ' }, 
+                         { ' ', ' ', ' ' }, 
+                         { ' ', ' ', ' ' } };
+    drawBoard(board);
+    return 0;
+
+    char player = 'X';
+    int row, col;
+    int turn;
+
+
+    
+}
+
